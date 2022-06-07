58bd57e76a5864a77d531205483c8f4d590cc05f
minju412 <ln8847@ajou.ac.kr>
2022-06-07 16:48:36 +0900

    @Autowired 필드명, @Qualifier, @Primary

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,14 +4,22 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor // final 변수의 생성자를 자동으로 만들어준다.
+//@RequiredArgsConstructor // final 변수의 생성자를 자동으로 만들어준다.
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository; // 생성자 주입을 사용하면 final 키워드를 넣을 수 있다는 장점이 있다.
     private final DiscountPolicy discountPolicy;
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
