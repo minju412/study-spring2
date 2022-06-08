7cdafa9cc8692550088178822d57760137d7db20
minju412 <ln8847@ajou.ac.kr>
2022-06-09 01:06:59 +0900

    스코프와 Provider

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,12 +14,13 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody // view 화면 없이 문자 반환
     public String logDemo(HttpServletRequest request){
         String requestURL = request.getRequestURL().toString(); // http request 정보를 받을 수 있다.
+        MyLogger myLogger = myLoggerProvider.getObject(); // Provider -> http request가 들어왔을 때 호출
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
--- src/main/java/hello/core/web/LogDemoService.java
+++ src/main/java/hello/core/web/LogDemoService.java
@@ -2,15 +2,17 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final MyLogger myLogger; // 의존관계를 주입받는다.
+    private final ObjectProvider<MyLogger> myLoggerProvider;; // 의존관계를 주입받는다.
 
     public void logic(String id) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
